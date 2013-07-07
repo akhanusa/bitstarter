874a4b53d41f7d20bbb07083b581475f64ca8023
Abid Khan <akhanusa@gmail.com>
2013-07-07 04:03:22 +0000

    index.html output part 1

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+fs = require('fs')
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fs.readFileSync("index.html"));
 });
 
 var port = process.env.PORT || 5000;
