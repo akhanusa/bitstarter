276a53fa937ab22ce9632b573a3cfb157d37ece2
akhanusa <akhanusa@gmail.com>
2013-07-25 02:42:43 +0000

    Edited web.js for utf-8

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  response.send(fs.readFileSync("index.html", 'utf-8'));
 });
 
 var port = process.env.PORT || 8080;
